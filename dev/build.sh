ecbb83c4f427755d73658dcad0a1d06bf7fb3b72
Christian Nikolov <christian@newton.codes>
2018-01-17 22:44:01 +0200

    Fixed build command argument

--- dev/build.sh
+++ dev/build.sh
@@ -4,8 +4,8 @@ dir=$(dirname "$0")
 v="$1"
 
 if [ "$v" = "" ] || [ "$v" = "16.04" ]; then
-    cd ${dir}/../16.04 && docker build -t newtoncodes/ubuntu
-    cd ${dir}/../16.04 && docker build -t newtoncodes/ubuntu:16.04
+    cd ${dir}/../16.04 && docker build -t newtoncodes/ubuntu .
+    cd ${dir}/../16.04 && docker build -t newtoncodes/ubuntu:16.04 .
 else
-    cd ${dir}/../${v} && docker build -t newtoncodes/ubuntu:${v}
+    cd ${dir}/../${v} && docker build -t newtoncodes/ubuntu:${v} .
 fi
